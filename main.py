50abc452e9db37292bfe050edc678b859acdbd7d
Adrian Chong <adrian.shun1387@outlook.com>
2025-04-07 22:21:42 +0930

    game loop

--- main.py
+++ main.py
@@ -5,6 +5,22 @@ def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    running = True 
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        
+        screen.fill("black")
+
+        pygame.display.flip() 
+
+        clock.tick(60)
+
 
 if __name__ == "__main__":
     main()
