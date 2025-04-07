3e4c93c0aed2c4fdcbe54635bc1ecec06dd22b83
Adrian Chong <adrian.shun1387@outlook.com>
2025-04-07 23:55:51 +0930

    rotation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     print("Starting Asteroids!")
@@ -8,20 +9,29 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    test_player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2) 
+    dt = 0
     running = True 
 
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                return
+                running = False
+        
         
         screen.fill("black")
-
-        pygame.display.flip() 
-
+        test_player.update(dt)
+        test_player.draw(screen)
+        
+        #this actually draws everything to the screen, right now we are drawing evertyhing off screen then 'flip' the whole thing and draw everything at once to the screen
+        #this is better for game smoothness, no flickering and more efficiency 
+        pygame.display.flip()
+        
+        
         clock.tick(60)
-
-
+        dt = clock.tick(60) / 1000 #uses less resources as we decouple games speed from the speed its being drawing to the screen 
+        
+  
 if __name__ == "__main__":
     main()
 
